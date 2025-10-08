c6fd4181a7971ad61b908e1e2c530f474492a966
AntonMartic <anton.martic@gmail.com>
2025-10-08 17:54:01 +0200

    listen to key inputs

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,16 @@
 from pynput import keyboard
+
+# OBS: allow IDE Input Monitoring, Accessibility, Full Disk Access in OS settings
+
+def on_press(key):
+    try:
+        print(key)
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
+
+
